7d3c0d6ea3bf092fbf131878d43aa6885a51e905
unknown <raajeshwaribarman@gmail.com>
2020-04-22 23:26:45 +0530

    POI added

---
+++ src/test/java/ExcelReading/TestDataReading.java
@@ -0,0 +1,76 @@
+package ExcelReading;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TestDataReading {
+	
+	public ArrayList<String> getdata(String data) throws IOException
+	{
+		FileInputStream file = new FileInputStream(
+				"C:\\Users\\Rajeshwari\\eclipse-workspace\\" + "ExcelReading\\Reading Data from Excel.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+		int no_sheet = workbook.getNumberOfSheets();
+		ArrayList<String> test_data = new ArrayList<String>();
+		for (int i = 0; i < no_sheet; i++) {
+			if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				// identifying the first row to scan the header
+				Iterator<Row> rows = sheet.rowIterator();
+				Row firstrow = rows.next();// get all the cells of the first row
+				Iterator<Cell> cells = firstrow.cellIterator();
+				int k = 0;
+				int col_num = 0;
+				while (cells.hasNext()) {
+					Cell value = cells.next();
+					if (value.getStringCellValue().trim().equalsIgnoreCase("testcase")) {
+						// desired column number
+						col_num = k;
+						break;
+					}
+					k++;
+				}
+				// Now the we need to locate the row whose testdata we have to read:
+				while (rows.hasNext()) {
+					Row row = rows.next();
+					if (row.getCell(col_num).getStringCellValue().equalsIgnoreCase(data)) {
+						// read the data of a particular row with a coulumm (add profile name)
+						System.out.println(row.getCell(1));
+						// now read all the cell data for that homepage Tc
+						Iterator<Cell> td = row.cellIterator();
+
+						while (td.hasNext()) {
+							//Cell cell = td.next();
+							//System.out.print(td.next());
+							//if(td.next().getCellTypeEnum() == CellType.STRING)
+								test_data.add(td.next().getStringCellValue());
+							//else
+								//test_data.add(NumberToTextConverter.toText(cell.getNumericCellValue()));
+							}
+						
+					}
+				}
+			}
+		}
+		return (test_data);
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		TestDataReading s1 = new TestDataReading();
+		ArrayList<String> test_data = s1.getdata("logout");
+		System.out.println(test_data);
+		
+	}
+
+}
